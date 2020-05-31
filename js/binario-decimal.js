8e066df3204ce8cbb97f650568880c330832ebed
José Daniel <j.g.santos.1998@gmail.com>
2020-05-31 19:38:23 -0300

    adicionado a propriedade min, max e maxlength para tentar limitar os valores colocados no input para 0 e 1 e até 8 caracteres mas só funciona com o type igual a text

--- js/binario-decimal.js
+++ js/binario-decimal.js
@@ -2,9 +2,10 @@
     
     //document.getElementById("decimal").innerHTML = bin2dec(POST);
     
-
+    
     function bin2dec(){
         let bin = document.getElementById("bin").value;
+
         document.getElementById("decimal").innerHTML = parseInt(bin, 2).toString(10);
         document.getElementById("binario").innerHTML = bin;
     }
