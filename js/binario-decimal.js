8419d78934a17e89dea9dc8038b89a14d48a9cfc
José Daniel <j.g.santos.1998@gmail.com>
2020-05-31 11:33:49 -0300

    adicionando um mini histórico para visualizaçao o número binário e decimal

--- js/binario-decimal.js
+++ js/binario-decimal.js
@@ -6,6 +6,6 @@
     function bin2dec(){
         let bin = document.getElementById("bin").value;
         document.getElementById("decimal").innerHTML = parseInt(bin, 2).toString(10);
+        document.getElementById("binario").innerHTML = bin;
     }
-    
    //console.log(typeof(parseInt(101, 2).toString(10))); 
