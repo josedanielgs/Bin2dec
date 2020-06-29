978f2e079efc5f2b83591cfa6521f014ee3dd822
José Daniel <j.g.santos.1998@gmail.com>
2020-06-29 10:17:35 -0300

    adicionado a capacidade de apresentar todas os números binários digitados e seus correspondentes na base 10 em um mini hitórico - ainda é uma funcionalidade em desenvolvimento, falta ajustar o estilo.

--- js/binario-decimal.js
+++ js/binario-decimal.js
@@ -4,9 +4,32 @@
     
     
     function bin2dec(){
+    // jQuery para validar se o texto é de números ou outros caracteres
         let bin = document.getElementById("bin").value;
+        let dec = parseInt(bin, 2).toString(10);
+        let listaBin = [];
+        let listaDec = [];
+        listaBin.push(bin);
+        listaDec.push(dec);
 
-        document.getElementById("decimal").innerHTML = parseInt(bin, 2).toString(10);
-        document.getElementById("binario").innerHTML = bin;
+        for(var i=0; i<listaBin.length; i++){    
+            let newItemDec = document.createElement("div");
+            let textDec = document.createTextNode(listaDec[i]);
+            newItemDec.appendChild(textDec);
+            let divP = document.getElementById("ok");
+
+            divP.insertBefore(newItemDec,divP.childNodes[i]);
+            
+            let newItemBin = document.createElement("div",);
+            let textBin = document.createTextNode(listaBin[i]);
+            newItemBin.appendChild(textBin);
+
+            divP.insertBefore(newItemBin,divP.childNodes[i]);
+
+            /*document.getElementById("prin").insertAdjacentHTML("afterend", "<div id= 'decimal' style= 'color: white; position: relative; right: 25%; margin-top: 15%;'></div>" );
+            document.getElementById("decimal").innerHTML = listaDec[i]; 
+            document.getElementById("prin").insertAdjacentHTML("afterend", "<div id='binario' style='color: white; position: relative; right: 66%; margin-top: 15%;'></div>");
+            document.getElementById("binario").innerHTML = listaBin[i];*/
+        }
     }
    //console.log(typeof(parseInt(101, 2).toString(10))); 
